a39689f65760dc6d40e67e45c0fefef27cd8e91d
JHONNY <jonnygarces2007@gmail.com>
2026-06-14 13:28:30 -0500

    Add main.py for OOP implementation with Mascota
    Implement main program using Object-Oriented Programming with Mascota class.

---
+++ programacion_poo/main.py
@@ -0,0 +1,12 @@
+# Programa principal utilizando Programación Orientada a Objetos
+
+from mascota import Mascota
+
+mascota1 = Mascota("Firulais", "Perro", 4)
+mascota2 = Mascota("Michi", "Gato", 2)
+
+mascota1.mostrar_informacion()
+mascota1.hacer_sonido()
+
+mascota2.mostrar_informacion()
+mascota2.hacer_sonido()
